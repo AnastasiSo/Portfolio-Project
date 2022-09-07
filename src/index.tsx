4125e861da3551ddd57592b2a6a2285c546bc8db
TaniaZaslon <zaslontania@gmail.com>
2022-09-07 09:04:30 +0200

    Setup App.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,4 @@
 import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <div> This is a new react app2</div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
