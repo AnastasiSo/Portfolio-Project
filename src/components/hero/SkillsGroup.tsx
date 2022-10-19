96f311271641b5ad18e970188a7a339472f7eeb2
Anastasiia <anastasiiasorina@gmail.com>
2022-10-19 18:19:04 +0200

    add interface to SkillsGroup
    add interface to SkillsGroup

---
+++ src/components/hero/SkillsGroup.tsx
@@ -0,0 +1,24 @@
+import { FunctionComponent } from "react";
+interface ISkillsGroupProps {
+  title: string;
+  skills: string[];
+}
+const SkillsGroup: FunctionComponent<ISkillsGroupProps> = ({
+  title,
+  skills,
+}) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <ul className="ball">
+        {skills.map((item, index) => (
+          <li key={index}>
+            <span>{item}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SkillsGroup;
