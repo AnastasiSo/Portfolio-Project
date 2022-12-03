b3c504531519f4c20cedad370414193cba92aafa
Anastasiia <anastasiiasorina@gmail.com>
2022-12-03 16:50:50 +0000

    create  component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import "../src/_styles/index.scss";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      {/* <Menu />
+      <Hero>
+        <SocialMedia />
+      </Hero>
+      <Projects>
+        <Slaiders>
+          <Slider />
+        </Slaiders>
+      </Projects> 
+      <Projects>     
+        <Rest />
+      </Projects>
+      <Contacts />
+      <Footer /> */}
+    </div>
+  );
 };
 
 export default App;
