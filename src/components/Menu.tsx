a2e11ad452d374c55a062018a73f5b4cbfb84a74
Anastasiia <anastasiiasorina@gmail.com>
2023-01-17 12:58:32 +0000

    menu adaptability for different screen sizes

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,30 +5,42 @@ import Column from "./layout/Column";
 import Container from "./layout/Container";
 import Row from "./layout/Row";
 
+// TODO: rename file to Navigation
+// TODO: 1.menu media query Sm, Md, Lg
+// TODO: 2.mobile menu functional
+
 const Menu: FunctionComponent = () => {
   const menuItems = ["About", "Projects", "Contacts", "Resume"];
 
   return (
     <Container>
       <Row>
-        <Column size={4} sizeLG={5}>
-          <div className="logo"></div>
+        <Column size={6} sizeSM={2} sizeLG={5}>
+          <a className="logo" href="#">
+            <img src="../img/logo.png" className="icon-logo" />
+          </a>
         </Column>
-        <Column size={8} sizeLG={7}>
-          <ul className="menu">
-            {menuItems.map((item, index) => (
-              <li
-                key={index}
-                className={`menu-items ${
-                  index === menuItems.length - 1 ? "resume" : ""
-                }`}
-              >
-                <a>
-                  <span>{item}</span>
-                </a>
-              </li>
-            ))}
-          </ul>
+        <Column size={6} sizeSM={10} sizeLG={7}>
+          <div className="mobile-menu">
+            <span className="mobile-menu-button">MENU</span>
+            {/* <span className="close-button">close</span>  TODO: create 'close-button' component? */}
+          </div>
+          <nav className="top-menu">
+            <ul className="menu">
+              {menuItems.map((item, index) => (
+                <li
+                  key={index}
+                  className={`menu-items ${
+                    index === menuItems.length - 1 ? "resume" : ""
+                  }`}
+                >
+                  <a href="#" className="menu-link">
+                    <span>{item}</span>
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
         </Column>
       </Row>
     </Container>
