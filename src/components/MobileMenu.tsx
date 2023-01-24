3bdb032e90e40742eddb74c64e2471e782649452
Anastasiia <anastasiiasorina@gmail.com>
2023-01-24 10:05:43 +0000

    add functionality to MENU and CLOSE button

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,31 +1,44 @@
+import { Fragment, FunctionComponent, useState } from "react";
 import "../_styles/index.scss";
 import Column from "./layout/Column";
 import Row from "./layout/Row";
-
-import { Fragment, FunctionComponent } from "react";
 import Menu from "./Menu";
 
 // TODO: 1.menu media query Sm, Md, Lg
 // TODO: 2.mobile menu functional
 
 const MobileMenu: FunctionComponent = () => {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  const handlerOnClickMenu = () => {
+    setShowMobileMenu(!showMobileMenu);
+  };
   return (
     <Fragment>
-      <button className="mobile-menu-button">MENU</button>
-      <div className="mobile-menu">
-        <Row>
-          <Column size={6} className="logo-wrap">
-            <a className="logo" href="#">
-              <img src="../img/logo-mobile.png" className="icon-logo" />
-            </a>
-          </Column>
-          <Column size={6}>
-            <span className="mobile-menu-close">CLOSE</span>
-          </Column>
-        </Row>
-        <Menu />
-        {/* <SocialMedia /> */}
-      </div>
+      <button onClick={handlerOnClickMenu} className="mobile-menu-button">
+        MENU
+      </button>
+      {showMobileMenu && (
+        <div className="mobile-menu">
+          <Row>
+            <Column size={6} className="logo-wrap">
+              <a className="logo" href="#">
+                <img src="../img/logo-mobile.png" className="icon-logo" />
+              </a>
+            </Column>
+            <Column size={6}>
+              <button
+                onClick={handlerOnClickMenu}
+                className="mobile-menu-close"
+              >
+                CLOSE
+              </button>
+            </Column>
+          </Row>
+          <Menu />
+          {/* <SocialMedia /> */}
+        </div>
+      )}
     </Fragment>
   );
 };
