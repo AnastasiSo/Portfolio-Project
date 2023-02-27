63edf547fc42aae4f808989d6709dac2848ff7f0
Anastasiia <anastasiiasorina@gmail.com>
2023-02-27 10:52:49 +0000

    add Slider component to Project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,11 +1,12 @@
 import Container from "@components/layout/Container";
+import Slider from "@components/Slider";
 import { FunctionComponent } from "react";
 
 const Projects: FunctionComponent = () => {
   return (
     <Container className="projects">
       <h2>Projects</h2>
-      {/* <Slider /> */}
+      <Slider />
     </Container>
   );
 };
