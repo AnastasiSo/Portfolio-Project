58f189274f0817fcde401a6ed72e08d5bcb69f17
TaniaZaslon <zaslontania@gmail.com>
2023-05-10 10:34:43 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
-// import Contacts from "@components/Contacts";
-// import Footer from "@components/Footer";
-// import Hero from "@components/Hero";
-// import Navigation from "@components/Navigation";
-// import Projects from "@components/Projects";
 import AboutMe from "@components/AboutMe";
+import Contacts from "@components/Contacts";
+import Footer from "@components/Footer";
+import Hero from "@components/Hero";
+import Navigation from "@components/Navigation";
+import Projects from "@components/Projects";
 import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
 import "../src/_styles/index.scss";
 
 const App = () => {
   return (
     <Fragment>
-      <AboutMe />
-      {/* <Navigation />
-      <Hero />
-      <Projects />
-      <Contacts />
-      <Footer /> */}
+      <Routes>
+        {/* // TODO: create folder "pages" with components HomePage and AboutMePage. */}
+        {/* Use HomePage component for path="/" */}
+        <Route
+          path="/"
+          element={
+            <Fragment>
+              <Navigation />
+              <Hero />
+              <Projects />
+              <Contacts />
+              <Footer />
+            </Fragment>
+          }
+        />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="*" element={<div>NoMatch </div>} />
+      </Routes>
     </Fragment>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
