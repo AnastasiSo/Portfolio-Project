6dfb8a6fdad8ed2e04fb64f2628a3749fca44032
Anastasiia Sorina <anastasiiasorina@gmail.com>
2023-05-16 21:38:53 +0100

    fix Displaying the contact component on the page of the account.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,14 @@
 import Contacts from "@components/Contacts";
 import Hero from "@components/Hero";
 import Projects from "@components/projects/Projects";
-import { Fragment } from "react";
 
 const HomePage = () => {
   return (
-    <Fragment>
+    <div className="home-page">
       <Hero />
       <Projects />
       <Contacts />
-    </Fragment>
+    </div>
   );
 };
 
