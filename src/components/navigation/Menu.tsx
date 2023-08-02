8cdfdf492819a475a770138c29756dbcf187f360
Anastasiia Sorina <anastasiiasorina@gmail.com>
2023-08-02 22:53:42 +0100

    add 404 page

--- src/components/navigation/Menu.tsx
+++ src/components/navigation/Menu.tsx
@@ -3,46 +3,44 @@ import { NavLink } from "react-router-dom";
 
 // TODO: Find a solution for the behavior of contacts buttons when the checkout page is active.
 
-const Menu: FunctionComponent = () => {
-  return (
-    <nav className="menu">
-      <ul className="menu-wrap">
-        <li className="menu-items">
-          <NavLink to="/" className="menu-link">
-            <span>Home</span>
-          </NavLink>
-        </li>
+const Menu: FunctionComponent = () => (
+  <nav className="menu">
+    <ul className="menu-wrap">
+      <li className="menu-items">
+        <NavLink to="/" className="menu-link">
+          <span>Home</span>
+        </NavLink>
+      </li>
 
-        <li className="menu-items">
-          <NavLink to="/about" className="menu-link">
-            <span>About</span>
-          </NavLink>
-        </li>
+      <li className="menu-items">
+        <NavLink to="/about" className="menu-link">
+          <span>About</span>
+        </NavLink>
+      </li>
 
-        <li className="menu-items home-page">
-          <a href="/#projects" className="menu-link">
-            <span>Projects</span>
-          </a>
-        </li>
+      <li className="menu-items home-page">
+        <a href="/#projects" className="menu-link">
+          <span>Projects</span>
+        </a>
+      </li>
 
-        <li className="menu-items">
-          <a href="/#contacts" className="menu-link">
-            <span>Contacts</span>
-          </a>
-        </li>
+      <li className="menu-items">
+        <a href="/#contacts" className="menu-link">
+          <span>Contacts</span>
+        </a>
+      </li>
 
-        <li className="menu-items">
-          <a
-            href="resumePDF"
-            download="AnastasiiaSorinaFrontEndDeveloperCV.pdf"
-            className="menu-link resume"
-          >
-            <span>Resume</span>
-          </a>
-        </li>
-      </ul>
-    </nav>
-  );
-};
+      <li className="menu-items">
+        <a
+          href="resumePDF"
+          download="AnastasiiaSorinaFrontEndDeveloperCV.pdf"
+          className="menu-link resume"
+        >
+          <span>Resume</span>
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Menu;
--- src/pages/NoMachPage.tsx
+++ src/pages/NoMachPage.tsx
@@ -1,11 +1,26 @@
-import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
+import Container from "../components/layout/Container";
+import CircularButton from "../components/navigation/CircularButton";
 
 const NoMachPage = () => {
+  const navigate = useNavigate();
+  const handleScrollToHomePage = () => {
+    navigate(-1);
+  };
   return (
-    <Fragment>
-      <h1></h1>
-      <p></p>
-    </Fragment>
+    <Container className="no-mach-page">
+      <h1>404</h1>
+      <h3>
+        Oops, we can't seem to find
+        <br />
+        the page what are you looking for.
+      </h3>
+      <p>Don't worry, you can return to the previous page.</p>
+      <CircularButton
+        direction="left"
+        handlerOnClick={handleScrollToHomePage}
+      />
+    </Container>
   );
 };
 
