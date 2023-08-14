0269f3fa78ee81f54664e84d7c243ee4b1585ada
Anastasiia Sorina <anastasiiasorina@gmail.com>
2023-08-14 23:43:43 +0100

    Implemented a page layout for the Rest API project

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,14 @@
 import Contacts from "@components/Contacts";
 import Hero from "@components/Hero";
 import Projects from "@components/projects/Projects";
+import RestAPI from "../components/projects/RestAPIProject";
 
 const HomePage = () => {
   return (
     <div className="home-page">
       <Hero />
       <Projects />
+      <RestAPI />
       <Contacts />
     </div>
   );
