210cf5389bcb154d4de80ee5117bbdd7c6a4ea8e
Anastasiia Sorina <anastasiiasorina@gmail.com>
2024-02-09 11:44:57 +0000

    add component  searcImages API

--- src/components/projects/API Project/SearchField.tsx
+++ src/components/projects/API Project/SearchField.tsx
@@ -1,28 +1,35 @@
 import axios from "axios";
-import { FunctionComponent, useEffect, useRef } from "react";
+import { FunctionComponent, Key, useRef, useState } from "react";
 const API_URL = "https://api.unsplash.com/search/photos";
-interface ISearchField {
-  REACT_API_KEY: string;
-}
-const SearchField: FunctionComponent<ISearchField> = () => {
-  console.log("key", import.meta.env.REACT_API_KEY);
+const REACT_API_KEY = "UCnponr8TqMg0f4R0j2tYI5xpONs6HwdtccnCw8FVNY";
+const SearchField: FunctionComponent = () => {
   const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  useEffect(() => {
-    const getImages = async () => {
-      const { data } = axios.get(
-        `${API_URL}?query=${searchInput.current.value}&page=1&per_page=6`
-      );
-    };
-    getImages();
-  }, []);
+  const fetchImages = async () => {
+    try {
+      if (searchInput.current.value) {
+        setErrorMsg("");
+        const { data } = await axios.get(
+          `${API_URL}?query=${searchInput.current.value}&per_page=6&client_id=${REACT_API_KEY}`
+        );
+        setImages(data.results);
+      }
+    } catch (error) {
+      setErrorMsg("Error fetching images. Try again later.");
+      console.log(error);
+    }
+  };
 
-  const handlerSearch = (event) => {
+  const handlerSearch = (event: { preventDefault: () => void }) => {
     event.preventDefault();
     console.log(searchInput.current.value);
+    fetchImages();
   };
   return (
     <div className="rest-api-project">
+      {errorMsg && <p className="error-msg">{errorMsg}</p>}
       <form onSubmit={handlerSearch}>
         <input
           type="search"
@@ -33,6 +40,22 @@ const SearchField: FunctionComponent<ISearchField> = () => {
           <span>SEARCH</span>
         </button>
       </form>
+      <div className="images">
+        {images.map(
+          (image: {
+            id: Key;
+            urls: { small: string };
+            alt_description: string;
+          }) => (
+            <img
+              key={image.id}
+              src={image.urls.small}
+              alt={image.alt_description}
+              className="image"
+            />
+          )
+        )}
+      </div>
     </div>
   );
 };
